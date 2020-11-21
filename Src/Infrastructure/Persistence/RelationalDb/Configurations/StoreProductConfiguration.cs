0d144662a4ca25798a96c4f9b847c8b8c60dff0c
vitptosek <>
2020-11-21 20:05:23 +0100

    Added required packages Added Dependency Injection for Persistence layer for DbContext registration and options Added concrete DbContext handling auditable entities and included corresponding DbSets along with their Configurations as per case study
    Note: the concrete DbContext is based on Domain entities level and defined by interface from Application layer - this adds abstraction as Services only care about data contract and not how the Persistence is handled

---
+++ Src/Infrastructure/Persistence/RelationalDb/Configurations/StoreProductConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Domain.Entities.Relations;
+
+namespace Persistence.RelationalDb.Configurations {
+
+	public class StoreProductConfiguration : IEntityTypeConfiguration<StoreProduct> {
+		public void Configure(EntityTypeBuilder<StoreProduct> builder) {
+			builder.ToTable("StoreProducts");
+
+			builder.HasKey(store => store.Id); //TODO: think of using composite key instead and add Count property to relational table instead
+
+			builder.Property(store => store.Id).HasColumnName("Guid")
+												.IsRequired()
+												.ValueGeneratedOnAdd();
+
+			builder.HasOne(storeProduct => storeProduct.Store)
+					.WithMany(store => store.Products)
+					.HasForeignKey(storeProduct => storeProduct.StoreId)
+					.OnDelete(DeleteBehavior.Cascade);
+
+			builder.HasOne(storeProduct => storeProduct.Product)
+					.WithMany(product => product.Stores)
+					.HasForeignKey(storeProduct => storeProduct.ProductId)
+					.OnDelete(DeleteBehavior.Cascade);
+
+			#region Filters
+
+			builder.HasQueryFilter(storeProduct => !storeProduct.Store.IsDeleted);
+			builder.HasQueryFilter(storeProduct => !storeProduct.Product.IsDeleted);
+
+			#endregion
+		}
+	}
+}
---
+++ Src/Infrastructure/Persistence/RelationalDb/Extensions/ModelBuilderSeedExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+
+using Domain.Entities;
+using Domain.Entities.Relations;
+
+namespace Persistence.RelationalDb.Extensions {
+
+	public static class ModelBuilderSeedExtensions {
+		private static readonly DateTime _created = DateTime.Now;
+		private static readonly DateTime _deleted = _created.AddMinutes(5);
+
+		public static ModelBuilder SeedData(this ModelBuilder modelBuilder) {
+			//TODO: add seed data
+
+			return modelBuilder;
+		}
+	}
+}
