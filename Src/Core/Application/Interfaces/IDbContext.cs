0f247c5d7182bf082d958b22bc4db6ee42f42b56
vitptosek <>
2020-11-21 19:45:21 +0100

    Application layer based on Domain Persistence

---
+++ Src/Core/Application/Interfaces/IDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+using Domain.Entities;
+using Domain.Entities.Relations;
+
+namespace Application.Interfaces {
+
+	public interface IDbContext {
+
+		#region Tables
+
+		public DbSet<Store> Stores { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+		#endregion
+
+		#region Relations
+
+		public DbSet<StoreProduct> StoreProducts { get; set; }
+
+		#endregion
+
+		#region EF Core
+
+		public DatabaseFacade Database { get; }
+
+		public int SaveChanges();
+		public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+		public Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken);
+
+		#endregion
+
+	}
+}
