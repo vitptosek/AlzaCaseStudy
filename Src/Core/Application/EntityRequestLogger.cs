c3aa47e45380387e4c3edf354d5fa902b5e0040a
vitptosek <>
2020-11-22 09:21:24 +0100

    Added custom exception and extension for Entity requests Added support for Entity requests using exception and extension - mapping between Application and Domain objects, validating context and logging slow tasks

---
+++ Src/Core/Application/EntityRequestLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+namespace Application {
+	public class EntityRequestLogger<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse> {
+		private readonly Stopwatch _stopwatch;
+
+		private long DurationMs => _stopwatch.ElapsedMilliseconds;
+
+		public EntityRequestLogger() => _stopwatch = new Stopwatch();
+
+		public async Task<TResponse> Handle(TRequest requestToLog, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> task) {
+			_stopwatch.Start();
+			var response = await task();
+			_stopwatch.Stop();
+
+			if (DurationMs > 500) {
+				Console.WriteLine($"{typeof(TRequest).Name} took {DurationMs} ms - {requestToLog}");
+			}
+
+			return response;
+		}
+	}
+}
---
+++ Src/Core/Application/EntityRequestMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+
+using Domain.Entities;
+using Application.Entities.Dto;
+
+namespace Application {
+
+	public class EntityRequestMapping : Profile {
+		public EntityRequestMapping() {
+			#region Product
+
+			CreateMap<Product, ProductDto>()
+				.ForMember(dto => dto.ProductName, options => options.MapFrom(product => product.Name))
+				.ForMember(dto => dto.ProductPrice, options => options.MapFrom(product => product.Price))
+				.ForMember(dto => dto.ProductImgUri, options => options.MapFrom(product => product.ImgUri))
+				.ForMember(dto => dto.ProductDescription, options => options.MapFrom(product => product.Description));
+
+			CreateMap<Product, ProductIncludesDto>().IncludeBase<Product, ProductDto>()
+				.ForMember(dto => dto.ProductStores, options => options.MapFrom(product => product.Stores));
+
+			#endregion
+		}
+	}
+}
