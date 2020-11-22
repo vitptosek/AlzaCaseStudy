51b476df6716736d1461f51d7caa3194fdf084b6
vitptosek <>
2020-11-22 08:49:28 +0100

    Added Application DTOs for Domain entities Added packages that are going to be used and udpated folder folder hierarchy for CQRS

---
+++ Src/Core/Application/Entities/Dto/StoreDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Entities.Dto {
+
+	public class StoreDto {
+		public string Name { get; internal set; }
+		public string City { get; internal set; }
+
+		internal StoreDto() { }
+	}
+}
