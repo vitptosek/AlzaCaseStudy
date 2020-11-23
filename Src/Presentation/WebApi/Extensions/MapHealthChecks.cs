ad38f050c44c489499eb8aed2fa02db1edf985d3
vitptosek <>
2020-11-23 08:49:39 +0100

    Added support for Health checks  - Dependency Injection for DbContext checks  - Extension method for mapping endpoints for checks
    Added required packages and registering new service in Startup + README updated

---
+++ Src/Presentation/WebApi/Extensions/MapHealthChecks.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace WebApi.Extensions {
+
+	public static class EndpointExtensions {
+
+		public static IEndpointRouteBuilder MapHealthChecks(this IEndpointRouteBuilder endpoints) {
+			//total health
+			endpoints.MapHealthChecks("/health", new HealthCheckOptions {
+				AllowCachingResponses = false,
+			});
+
+			//health of all registered db contexts
+			endpoints.MapHealthChecks("/health/db", new HealthCheckOptions {
+				Predicate = check => check.Tags.Contains("db"),
+			});
+			//health of all registered db contexts belonging to Alza
+			endpoints.MapHealthChecks("/health/db/alza", new HealthCheckOptions {
+				Predicate = check => check.Tags.Contains("alza"),
+			});
+
+			return endpoints;
+		}
+	}
+}
